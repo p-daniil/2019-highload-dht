b2b66686fdd0333071cb00f03104e84147e97b52
Daniil <danilp2505@gmail.com>
2019-10-05 20:35:18 +0300

    fixed server

--- src/main/java/ru/mail/polis/service/SimpleHttpServer.java
+++ src/main/java/ru/mail/polis/service/SimpleHttpServer.java
@@ -18,21 +18,23 @@ public class SimpleHttpServer extends HttpServer implements Service {
         this.dao = dao;
     }
 
+    @Path("/v0/status")
+    @RequestMethod(Request.METHOD_GET)
+    public Response status(final Request request) {
+        return new Response(Response.OK, Response.EMPTY);
+    }
+
     @Path("/v0/entity")
     public Response entity(@Param("id") final String id, final Request request) {
-        if (id == null) {
-            return new Response(Response.BAD_REQUEST);
+
+        if (id == null || id.isEmpty()) {
+            return new Response(Response.BAD_REQUEST, Response.EMPTY);
         }
         final ByteBuffer key = ByteBuffer.wrap(id.getBytes(Charsets.UTF_8));
         try {
             switch (request.getMethod()) {
                 case Request.METHOD_GET: {
-                    final ByteBuffer value;
-                    try {
-                        value = dao.get(key);
-                    } catch (NoSuchElementException e) {
-                        return new Response(Response.NOT_FOUND, "Key not found".getBytes(Charsets.UTF_8));
-                    }
+                    final ByteBuffer value = dao.get(key);
                     final ByteBuffer duplicate = value.duplicate();
                     final byte[] body = new byte[duplicate.remaining()];
                     duplicate.get(body);
@@ -40,22 +42,24 @@ public class SimpleHttpServer extends HttpServer implements Service {
                 }
                 case Request.METHOD_PUT: {
                     dao.upsert(key, ByteBuffer.wrap(request.getBody()));
-                    return new Response(Response.CREATED);
+                    return new Response(Response.CREATED, Response.EMPTY);
                 }
                 case Request.METHOD_DELETE: {
                     dao.remove(key);
-                    return new Response(Response.ACCEPTED);
+                    return new Response(Response.ACCEPTED, Response.EMPTY);
                 }
                 default: {
-                    return new Response(Response.METHOD_NOT_ALLOWED);
+                    return new Response(Response.METHOD_NOT_ALLOWED, Response.EMPTY);
                 }
             }
         } catch (IOException e) {
-            return new Response(Response.INTERNAL_ERROR);
+            return new Response(Response.INTERNAL_ERROR, Response.EMPTY);
+        } catch (NoSuchElementException e) {
+            return new Response(Response.NOT_FOUND, "Key not found".getBytes(Charsets.UTF_8));
         }
     }
 
-    public static HttpServerConfig getConfig(final int port) {
+    private static HttpServerConfig getConfig(final int port) {
         if (port <= 1024 || port >= 65535) {
             throw new IllegalArgumentException("Invalid port");
         }
@@ -64,5 +68,10 @@ public class SimpleHttpServer extends HttpServer implements Service {
         final HttpServerConfig serverConfig = new HttpServerConfig();
         serverConfig.acceptors = new AcceptorConfig[]{acceptorConfig};
         return serverConfig;
+    }
+
+    @Override
+    public void handleDefault(Request request, HttpSession session) throws IOException {
+        session.sendResponse(new Response(Response.BAD_REQUEST, Response.EMPTY));
     }
 }
