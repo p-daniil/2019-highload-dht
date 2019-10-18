2c320670e288459076d8370324b52392645bb082
Daniil <danilp2505@gmail.com>
2019-10-18 21:49:02 +0300

    working version

--- src/main/java/ru/mail/polis/service/StorageSession.java
+++ src/main/java/ru/mail/polis/service/StorageSession.java
@@ -1,6 +1,5 @@
 package ru.mail.polis.service;
 
-import com.google.common.base.Charsets;
 import one.nio.http.HttpServer;
 import one.nio.http.HttpSession;
 import one.nio.http.Response;
@@ -15,13 +14,13 @@ import java.util.Iterator;
 
 public class StorageSession extends HttpSession {
     private static final Logger LOG = LoggerFactory.getLogger(StorageSession.class);
-    private static final byte[] CRLF = "\r\n".getBytes(Charsets.UTF_8);
+    private static final byte[] CRLF = "\r\n".getBytes();
     private static final byte LF = '\n';
-    private static final byte[] EMPTY_CHUNK = "0\r\n\r\n".getBytes(Charsets.UTF_8);
+    private static final byte[] EMPTY_CHUNK = "0\r\n\r\n".getBytes();
 
     private Iterator<Record> records;
 
-    public StorageSession(Socket socket, HttpServer server) {
+    StorageSession(Socket socket, HttpServer server) {
         super(socket, server);
     }
 
@@ -31,7 +30,6 @@ public class StorageSession extends HttpSession {
         final Response response = new Response(Response.OK);
         response.addHeader("Transfer-Encoding: chunked");
         writeResponse(response, false);
-
         next();
     }
 
@@ -51,41 +49,43 @@ public class StorageSession extends HttpSession {
     private void next() throws IOException {
         while (records.hasNext() && queueHead == null) {
             final Record record = records.next();
-            final byte[] key = toByteArray(record.getKey());
-            final byte[] value = toByteArray(record.getValue());
+            writeChunk(record);
+        }
+        write(EMPTY_CHUNK, 0, EMPTY_CHUNK.length);
 
-            final int payloadLength = key.length + 1 + value.length;
-            final String size = Integer.toHexString(payloadLength);
+        server.incRequestsProcessed();
 
-            final int chunkLength = size.length() + 2 + payloadLength + 2;
-
-            final byte[] chunk = new byte[chunkLength];
-            final ByteBuffer buffer = ByteBuffer.wrap(chunk);
-            buffer.put(size.getBytes());
-            buffer.put(CRLF);
-            buffer.put(key);
-            buffer.put(LF);
-            buffer.put(value);
-            write(chunk, 0, chunkLength);
-
-            if (!records.hasNext()) {
-                write(EMPTY_CHUNK, 0, EMPTY_CHUNK.length);
-            }
-
-            server.incRequestsProcessed();
-
-            if ((handling = pipeline.pollFirst()) != null) {
-                if (handling == FIN) {
-                    scheduleClose();
-                } else {
-                    try {
-                        server.handleRequest(handling, this);
-                    } catch (IOException e) {
-                        LOG.error("Can't process next request: " + handling, e);
-                    }
-
+        if ((handling = pipeline.pollFirst()) != null) {
+            if (handling == FIN) {
+                scheduleClose();
+            } else {
+                try {
+                    server.handleRequest(handling, this);
+                } catch (IOException e) {
+                    LOG.error("Can't process next request: " + handling, e);
                 }
+
             }
         }
+    }
+
+    private void writeChunk(Record record) throws IOException {
+        final byte[] key = toByteArray(record.getKey());
+        final byte[] value = toByteArray(record.getValue());
+
+        final int payloadLength = key.length + 1 + value.length;
+        final String size = Integer.toHexString(payloadLength);
+
+        final int chunkLength = size.length() + 2 + payloadLength + 2;
+
+        final byte[] chunk = new byte[chunkLength];
+        final ByteBuffer buffer = ByteBuffer.wrap(chunk);
+        buffer.put(size.getBytes());
+        buffer.put(CRLF);
+        buffer.put(key);
+        buffer.put(LF);
+        buffer.put(value);
+        buffer.put(CRLF);
+        write(chunk, 0, chunkLength);
     }
 }
