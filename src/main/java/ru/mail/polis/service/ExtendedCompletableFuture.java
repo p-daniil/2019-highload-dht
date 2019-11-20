df86cbe12178f6ebefbd7a2b54804c63595ecd63
Daniil <danilp2505@gmail.com>
2019-11-20 20:55:58 +0300

    lock fix, added exceptionally

--- src/main/java/ru/mail/polis/service/ExtendedCompletableFuture.java
+++ src/main/java/ru/mail/polis/service/ExtendedCompletableFuture.java
@@ -1,6 +1,8 @@
 package ru.mail.polis.service;
 
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -12,20 +14,23 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.function.BiConsumer;
 
 class ExtendedCompletableFuture<T> extends CompletableFuture<T> {
-    private static final ReadWriteLock lock = new ReentrantReadWriteLock();
+    private static final Logger LOG = LoggerFactory.getLogger(ExtendedCompletableFuture.class);
 
-    @SuppressWarnings("FutureReturnValueIgnored")
     static <T> CompletableFuture<List<T>> firstN(final List<CompletableFuture<T>> list, final int n) {
         final int maxFail = list.size() - n;
         if (maxFail < 0) throw new IllegalArgumentException();
 
         final AtomicInteger fails = new AtomicInteger(0);
         final List<T> resultList = new ArrayList<>(n);
+        final ReadWriteLock lock = new ReentrantReadWriteLock();
 
         final CompletableFuture<List<T>> result = new CompletableFuture<>();
 
-        final BiConsumer<T, Throwable> c = getResultHandler(n, maxFail, fails, resultList, result);
-        for (final CompletableFuture<T> f : list) f.whenCompleteAsync(c);
+        final BiConsumer<T, Throwable> c = getResultHandler(n, maxFail, fails, resultList, lock, result);
+        for (final CompletableFuture<T> f : list) f.whenCompleteAsync(c).exceptionally(e -> {
+            LOG.error("Failed to handle result", e);
+            return null;
+        });
         return result;
     }
 
@@ -34,6 +39,7 @@ class ExtendedCompletableFuture<T> extends CompletableFuture<T> {
                                                                  final int maxFail,
                                                                  final AtomicInteger fails,
                                                                  final List<T> resultList,
+                                                                 final ReadWriteLock lock,
                                                                  final CompletableFuture<List<T>> future) {
         return (value, failure) -> {
             if (failure == null) {
